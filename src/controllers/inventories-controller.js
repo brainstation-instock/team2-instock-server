fdc11482c6b22a46c8bd0c0be98d7f69de565e8c
Roscoe Shum <shum.roscoe@gmail.com>
2024-03-28 11:41:57 -0400

    fixed get inventories

--- src/controllers/inventories-controller.js
+++ src/controllers/inventories-controller.js
@@ -2,15 +2,18 @@ const knex = require('knex')(require('../../knexfile'));
 
 module.exports.getInventories = async(_req, res) => {
     
-    const inventories = await knex('inventories');
+    const inventories = await knex('inventories')
+    .join("warehouses", "inventories.warehouse_id", "warehouses.id");
     res.json({inventories});
 }
 
 module.exports.addItem = async(req,res) => {
+    console.log(req.body)
 
     const result = await knex("inventories").insert(req.body);
     const newItemId = result[0];
-    const createdItem = await("inventories").where({id: newItemId});
+    
+    const createdItem = await knex("inventories").where({id: newItemId});
 
     res.status(201).json(createdItem);
 }
