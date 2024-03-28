8ab1f6597ca8b931a8df9f1d215b4559e1eb0578
Roscoe Shum <shum.roscoe@gmail.com>
2024-03-28 11:05:28 -0400

    paused to check out develop

--- src/controllers/inventories-controller.js
+++ src/controllers/inventories-controller.js
@@ -8,14 +8,52 @@ module.exports.getInventories = async(_req, res) => {
 
 module.exports.addItem = async(req,res) => {
 
+    if (!req.body.item_name || !req.body.description || !req.body.category || !req.body.quantity) {
+        return res.status(400).json({
+            message: 'All fields must be filled!'
+        })
+    }
+
+    function isNumber(str) {
+        return /^-?\d+$/.test(str);
+    }
+
+    if(isNumber(req.body.quantity)){
+        return res.status(400).json({
+            message: 'Quantity must be a valid number!'
+        })
+    }
+
+    try{
     const result = await knex("inventories").insert(req.body);
     const newItemId = result[0];
-    const createdItem = await("inventories").where({id: newItemId});
+    const createdItem = await knex("inventories").where({id: newItemId});
 
     res.status(201).json(createdItem);
+    } catch(error) {
+        res.status(500).json({
+            message: `Unable to add item: ${error}`
+        })
+    }
 }
 
 module.exports.editItem = async(req,res) => {
+
+    if (!req.body.item_name || !req.body.description || !req.body.category || !req.body.quantity) {
+        return res.status(400).json({
+            message: 'All fields must be filled!'
+        })
+    }
+
+    function isNumber(str) {
+        return /^-?\d+$/.test(str);
+    }
+
+    if(isNumber(req.body.quantity)){
+        return res.status(400).json({
+            message: 'Quantity must be a valid number!'
+        })
+    }
     
     try {
         const rowsUpdated = await knex("inventories")
@@ -28,6 +66,7 @@ module.exports.editItem = async(req,res) => {
             return res.status(404).json({
                 message: `Item with ID ${req.params.id} not found.`
             });
+        
         }
 
         const updatedItem = await knex("inventories")
--- src/controllers/warehouses-controller.js
+++ src/controllers/warehouses-controller.js
@@ -3,6 +3,6 @@ const knex = require('knex')(require('../../knexfile'));
 module.exports.getWarehouses = async(_req, res) => {
 
     const warehouses = await knex('warehouses');
-    res.json({warehouses});
+    res.status(200).json({warehouses});
 }
 
